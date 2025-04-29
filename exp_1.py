2339a39fd38e415e8878a6b59e9ddc8d45c1305d
Mahesh Prajapat <maheshprajapat45703@gmail.com>
2025-04-30 01:00:28 +0530

    Final commit

---
+++ exp_1.py
@@ -0,0 +1,41 @@
+# Python program to demonstrate data types
+
+# Integer
+integer_var = 10
+print("Integer:", integer_var, "->", type(integer_var))
+
+# Float
+float_var = 10.5
+print("Float:", float_var, "->", type(float_var))
+
+# String
+string_var = "Hello, Python!"
+print("String:", string_var, "->", type(string_var))
+
+# Boolean
+bool_var = True
+print("Boolean:", bool_var, "->", type(bool_var))
+
+# List
+list_var = [1, 2, 3, 4, 5]
+print("List:", list_var, "->", type(list_var))
+
+# Tuple
+tuple_var = (1, 2, 3)
+print("Tuple:", tuple_var, "->", type(tuple_var))
+
+# Set
+set_var = {1, 2, 3, 4}
+print("Set:", set_var, "->", type(set_var))
+
+# Dictionary
+dict_var = {"name": "Alice", "age": 25}
+print("Dictionary:", dict_var, "->", type(dict_var))
+
+# NoneType
+none_var = None
+print("NoneType:", none_var, "->", type(none_var))
+
+# Complex Number
+complex_var = 2 + 3j
+print("Complex:", complex_var, "->", type(complex_var))
